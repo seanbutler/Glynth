72798a5cf7daa7dc4641d84c9a5f68f2ff1dbf84
Sean Butler <butlersean@gmail.com>
2020-07-29 22:00:58 +0100

    data tweak for JSON export tests

--- Assets/agent2.c
+++ Assets/agent2.c
@@ -1,12 +1,9 @@
-direction = 3;
+direction = 2+1;
 
-while(1)
+moves = 8;
+while ( moves > 0 )
 {
-    moves = 8;
-    while ( moves > 0 )
-    {
-        moves = moves - 1;
-        move(direction);
-    }
-    direction = direction + 1;
+    moves = moves - 1;
+    move(direction);
 }
+direction = direction + 1;
